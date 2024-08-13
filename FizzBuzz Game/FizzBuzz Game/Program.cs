d9a95b6369449d268be890aeed006a39391ab2db
RonyGabriel94 <rgcb.1994@outlook.com>
2024-08-13 22:41:52 +0100

    Add files via upload

---
+++ FizzBuzz Game/FizzBuzz Game/Program.cs
@@ -0,0 +1,22 @@
+﻿// FizzBuzz game from 1 to a 100
+
+for (int i = 0; i<=101; i++)
+{
+
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else { Console.WriteLine(i); }
+    
+
+   
+}
